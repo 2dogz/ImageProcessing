a17217300b0c2d34e7a9b70b7043edb6411a969b
2dogz <kylewebb58@yahoo.com>
2022-02-12 20:25:36 -0600

    create basic flask frontend & python webcam taker to show on front end

---
+++ image_python/hello.py
@@ -0,0 +1,28 @@
+from flask import Flask, redirect, render_template
+from picture_helpers import take_pic
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html', title='Welcome')
+
+
+@app.route('/newPicture')
+def newPicture():
+    take_pic()
+    return redirect("/", code=302)
+
+
+@app.route('/client')
+def client():
+    return render_template('client.html', title='Clients')
+
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html', title='Contacts')
+
+@app.route('/about')
+def service():
+    return render_template('about.html', title='About')
---
+++ image_python/picture_helpers.py
@@ -0,0 +1,34 @@
+# program to capture single image from webcam in python
+# SOURCE : https://www.geeksforgeeks.org/how-to-capture-a-image-from-webcam-in-python/
+import cv2
+
+def take_pic():
+    # initialize the camera
+    # If you have multiple camera connected with
+    # current device, assign a value in cam_port
+    # variable according to that
+    cam_port = 0
+    cam = cv2.VideoCapture(cam_port)
+
+    # reading the input using the camera
+    result, image = cam.read()
+
+    # If image will detected without any error,
+    # show result
+    if result:
+
+        # showing result, it take frame name and image
+        # output
+        cv2.imshow("xyz", image)
+
+        # saving image in local storage
+        cv2.imwrite("static/xyz.png", image)
+
+        # If keyboard interrupt occurs, destroy image
+        # window
+        #cv2.waitKey(0)
+        #cv2.destroyWindow("xyz")
+
+    # If captured image is corrupted, moving to else part
+    else:
+        print("No image detected. Please! try again")
