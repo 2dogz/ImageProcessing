6a463d7d8e67d81d10f5516a04fa33bd51890cbe
2dogz <kylewebb58@yahoo.com>
2022-03-10 07:58:05 -0600

    fix syntax error, app.py

--- WAR3/website/app.py
+++ WAR3/website/app.py
@@ -47,7 +47,7 @@ def photos():
 @app.after_request
 def add_header(response):
     # response.cache_control.no_store = True
-    response.headers['Cache-Control'] = 'no-store, no-cache, must-
+    response.headers['Cache-Control'] = 'no-store, no-cache, must- \
     revalidate, post-check=0, pre-check=0, max-age=0'
     response.headers['Pragma'] = 'no-cache'
     response.headers['Expires'] = '-1'
