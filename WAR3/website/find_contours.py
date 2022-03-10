a94c9f23edf34da84b991e565b787dbf9941238d
2dogz <kylewebb58@yahoo.com>
2022-03-10 08:07:12 -0600

    update flask image code - test

--- WAR3/website/find_contours.py
+++ WAR3/website/find_contours.py
@@ -12,7 +12,7 @@ def contoursPicture():
         for data in contours:
             print("The contours have this data %r" %data)
         cv2.drawContours(image, contours, -1, (0, 255, 0), 3)
-        cv2.imwrite(f"static/photos/xyz.png", image)
+        cv2.imwrite(f"static/photos/xyz1.png", image)
     # If captured image is corrupted, moving to else part
     else:
         raise Exception("No image detected. Please! try again")
